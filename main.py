24faf295ac3e07904dcc9a305752ddb6887acf87
JammyJulia <julija.osipova@outlook.com>
2024-02-14 12:46:21 +0200

    finished

--- main.py
+++ main.py
@@ -16,16 +16,81 @@
 # [!] Programmai jaglabā izdevumu stāvokli kad programma ir izslēgta palaista no jauna
 #
 
-expenses = []
+import json
+
+
+with open("expenses.json",'r') as file:
+    expenses = json.load(file)
 
 # load expenses from expenses.json file here
 # https://www.geeksforgeeks.org/read-write-and-parse-json-using-python/ (Python read JSON file)
 pass
 
+def save():
+    with open("expenses.json", "w") as otherfile:
+        json.dump(expenses, otherfile)
+
+def sort(expense):
+    return int(expense["sum"])
+
 while True:
-    command = input("\nChoose command:")
+    command = input('''\nChoose command 
+1 - Add new expense
+2 - View all expenses
+3 - View 10 biggest expenses
+4 - View 10 smallest expenses
+5 - Avarage expense sum
+e - Exit
+Select: ''')
     if command == "1":
-        pass
+        name = input('Name: ')
+        while True:
+            try:
+                sum = round(float(input('Sum: ')), 2)
+                break
+            except:
+                print("Not a number!")
+        category = input('Category: ')
+        newexpense = {"name" : name, "sum" : sum, "category" : category}
+        try:
+            expenses.append(newexpense)
+        except AttributeError:
+            expenses = []
+            expenses.append(newexpense)
+        print(expenses)
+        save()
+    if command == "2":
+        for i in expenses:
+            print("-------------------")
+            print("Name:", i["name"])
+            print("Sum:", i["sum"])
+            print("Category:", i["category"])
+    if command == "3":
+        expenses.sort(key = sort, reverse=True)
+        for i in range(10):
+            try:
+                print("-------------------")
+                print("Name:", expenses[i]["name"])
+                print("Sum:", expenses[i]["sum"])
+                print("Category:", expenses[i]["category"])
+            except:
+                break
+    if command == "4":
+        expenses.sort(key = sort)
+        for i in range(10):
+            try:
+                print("-------------------")
+                print("Name:", expenses[i]["name"])
+                print("Sum:", expenses[i]["sum"])
+                print("Category:", expenses[i]["category"])
+            except:
+                break
+    if command == "5":
+        summa = 0
+        for i in expenses:
+            summa += i['sum']
+        avarage = summa/len(expenses)
+        print("Avarage", round(avarage, 2))
     if command == "e":
         print("Exiting...")
         break
